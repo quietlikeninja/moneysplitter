99c582cb10c8c1d784aed4c6177ec21e3d0cfb01
Justin Ward <justin@quietlike.ninja>
2022-10-16 21:09:16 +1000

    added input validation

--- money_splitter.py
+++ money_splitter.py
@@ -2,6 +2,12 @@ import numpy as np
 import tkinter
 import tkinter.messagebox
 
+def calculateSplit(pay):
+    splurge = np.round(float(pay) * 0.075, 2)
+    smile = np.round(float(pay) * 0.1, 2)
+    fire = np.round(float(pay) * 0.15, 2)
+    return splurge, smile, fire
+
 class MoneySplitterGUI: # define a ProgramGUI class
     def __init__(self): 
         self.splurge = 0
@@ -11,15 +17,15 @@ class MoneySplitterGUI: # define a ProgramGUI class
         self.main.title('Money Splitter')
 
         # create Frame widgets
-        self.top = tkinter.Frame(self.main, padx=8, pady=4)
+        self.input = tkinter.Frame(self.main, padx=8, pady=4)
         self.resultsSplurge = tkinter.Frame(self.main, padx=8, pady=4)
         self.resultsSmile = tkinter.Frame(self.main, padx=8, pady=4)
         self.resultsFire = tkinter.Frame(self.main, padx=8, pady=4)
-        self.bottom = tkinter.Frame(self.main, padx=8, pady=4)
+        self.buttons = tkinter.Frame(self.main, padx=8, pady=4)
 
         # create and pack the pay entry widget
-        self.payPrompt = tkinter.Label(self.top, width=25, justify='right', text='How much were you paid? ')
-        self.pay = tkinter.Entry(self.top, width=6)
+        self.payPrompt = tkinter.Label(self.input, width=25, justify='right', text='How much were you paid? ')
+        self.pay = tkinter.Entry(self.input, width=7)
         self.payPrompt.pack(side='left')
         self.pay.pack(side='right')
 
@@ -32,29 +38,33 @@ class MoneySplitterGUI: # define a ProgramGUI class
         self.fireResult.pack(side='left')
 
         # create and pack the button widgets
-        self.splitButton = tkinter.Button(self.bottom, text='Split Pay', command=self.showResult)
-        self.resetButton = tkinter.Button(self.bottom, text='Reset', command=self.resetForm)
+        self.splitButton = tkinter.Button(self.buttons, text='Split Pay', command=self.showResult)
+        self.resetButton = tkinter.Button(self.buttons, text='Reset', command=self.resetForm)
 
         self.splitButton.pack(side='left')
         self.resetButton.pack(side='left')
 
         # pack Frame widgets
-        self.top.pack()
+        self.input.pack(anchor='w')
         self.resultsSplurge.pack(anchor='w')
         self.resultsSmile.pack(anchor='w')
         self.resultsFire.pack(anchor='w')
-        self.bottom.pack()
+        self.buttons.pack()
 
         self.pay.focus_set()
 
         tkinter.mainloop()
 
     def showResult(self):
-        self.splurge = np.round(float(self.pay.get()) * 0.075, 2)
-        self.smile = np.round(float(self.pay.get()) * 0.1, 2)
-        self.fire = np.round(float(self.pay.get()) * 0.15, 2)
-        self.changeText()
-        #tkinter.messagebox.showinfo('Results', f"""Splurge: ${self.splurge}\n Smile: ${self.smile}\nFire: ${self.fire}""")
+        try:
+            if float(self.pay.get()) <= 0:
+                raise ValueError
+            else:
+                self.splurge, self.smile, self.fire = calculateSplit(self.pay.get())
+                self.changeText()
+                #tkinter.messagebox.showinfo('Results', f"""Splurge: ${self.splurge}\n Smile: ${self.smile}\nFire: ${self.fire}""")
+        except ValueError:
+            tkinter.messagebox.showerror('Invalid Input', f'Enter positive numbers only')     
 
     def resetForm(self):
         self.splurge = 0
@@ -70,14 +80,3 @@ class MoneySplitterGUI: # define a ProgramGUI class
         self.fireResult['text'] = f'Fire: ${self.fire}'
 
 gui = MoneySplitterGUI() # create a ProgramGUI object
-
-# income = input("How much were you paid? ")
-
-# splurge = np.round(float(income) * 0.075, 2)
-# smile = np.round(float(income) * 0.1, 2)
-# fire = np.round(float(income) * 0.15, 2)
-
-# print("Transfer Amounts:")
-# print("Splurge: $" + str(splurge))
-# print("Smile: $" + str(smile))
-# print("Fire Extinguisher: $" + str(fire))
